3f265cf9a0dd4d92e5e2a17ae5e9d94f46693044
vagrant <santhoshj025@gmail.com>
2017-02-16 17:35:40 +0000

    first commit

---
+++ js/exportToGraph.js
@@ -0,0 +1,34 @@
+let neo4j = require('neo4j-driver').v1;
+var driver = neo4j.driver("bolt://192.168.1.116", neo4j.auth.basic("neo4j", "santh205554"));
+let query = `MATCH (n:player{name:'nehra'})-[s:knows]->(m:player{name:'yuvi'}) return n,m,s`;
+// let session = driver().session();
+
+// Register a callback to know if driver creation was successful:
+// driver.onCompleted = function() {
+//   // proceed with using the driver, it was successfully instantiated
+//   console.log("successfully instantiated");
+// };
+//
+// // Register a callback to know if driver creation failed.
+// // This could happen due to wrong credentials or database unavailability:
+// driver.onError = function(error) {
+//   console.log('Driver instantiation failed', error);
+// };
+
+// Create a session to run Cypher statements in.
+// Note: Always make sure to close sessions when you are done using them!
+var session = driver.session();
+session
+    .run(query)
+    .then(function(result) {
+
+        session.close();
+        console.log(query);
+        console.log(result);
+        driver.close();
+
+    })
+    .catch(function(error) {
+        console.log(error);
+        driver.close();
+    });
